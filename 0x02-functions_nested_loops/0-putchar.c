5206b0a32abf93ecd8577f6a26a5017c0ec5759a
Darshna <officialdarshna@gmail.com>
2022-06-07 11:41:20 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,7 +5,8 @@
  */
 int main(void)
 {
-	_putchar('_');
+	char character = "_";
+	_putchar(character);
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
@@ -13,7 +14,7 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
-	_putchar('\r');
+	_putchar('\n');
 
 	return (0);
 }
