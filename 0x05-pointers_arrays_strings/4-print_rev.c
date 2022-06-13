e2291d4e1a5f36a0c32c2431773173fae3b0c7fd
Darshna <officialdarshna@gmail.com>
2022-06-13 12:10:47 -0700

    Task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_rev - prints a string in reverse, followed by a new line
+ * print_rev - prints a string in reverse
  * @s: string to be printed
  */
 void print_rev(char *s)
