50ba3e1a8ea47ffbe39a2627302e2a3cf19ae4cc
Darshna <officialdarshna@gmail.com>
2022-07-06 11:50:45 -0700

    Task 4

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -9,7 +9,7 @@
 int main(int agrc, char *argv[])
 {
 	int bytes, i;
-	char *arr;
+	char *arr = (char *) main;
 
 	if (argc != 2)
 	{
@@ -25,16 +25,13 @@ int main(int agrc, char *argv[])
 		exit(2);
 	}
 
-	arr = (char *) main;
-
 	for (i = 0; i < bytes; i++)
 	{
-		if (i == bytes - 1)
-		{
-			printf("%02hhx\n", arr[i]);
-			break;
-		}
-		printf("%02hhx ", arr[i]);
+		printf("%02x", arr[i] & 0xFF);
+		if (i != bytes - 1)
+			printf(" ");
 	}
+
+	printf("\n");
 	return (0);
 }
