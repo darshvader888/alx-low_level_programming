26f057c4d62205e62064146547fe7fc89d4c0bbd
Darshna <officialdarshna@gmail.com>
2022-07-26 06:53:09 -0700

    Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,8 +20,8 @@ int create_file(const char *filename, char *text_content)
 	{
 		while (text_content[s])
 			s++;
-		t = write(fd, text_content, s):
-			if (t != s)
+		t = write(fd, text_content, s);
+		if (t != s)
 			return (-1);
 	}
 
